231f16a2d9adad179165eaaef7503869493869c6
m-allanson <michael.allanson@gmail.com>
2021-12-01 22:30:25 +0000

    day 1: Less adding

--- day-01/02.ts
+++ day-01/02.ts
@@ -7,11 +7,19 @@ const values = data.split("\n").map((i) => parseInt(i, 10));
 
 let answer = 0;
 
+// /* First attempt */
+// for (let i = 0; i < values.length; i++) {
+//   const [a, b, c, d] = [values[i], values[i + 1], values[i + 2], values[i + 3]];
+//   if (d === undefined) break;
+//   /* b + c are the same in both sums, so we could just remove them */
+//   const first = a + b + c;
+//   const second = b + c + d;
+//   if (second - first > 0) answer += 1;
+// }
+
 for (let i = 0; i < values.length; i++) {
-  const [a, b, c, d] = [values[i], values[i + 1], values[i + 2], values[i + 3]];
-  if (d === undefined) break;
-  const first = a + b + c;
-  const second = b + c + d;
+  const [first, second] = [values[i], values[i + 3]];
+  if (second === undefined) break;
   if (second - first > 0) answer += 1;
 }
 
